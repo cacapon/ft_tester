ce29018ebb142058338ebf30f6738c3687f8d581
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-10-31 19:58:45 +0900

    add test_ft_substr

---
+++ test/libft/template.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+#include "unity.h"
+
+void	setUp(void)
+{
+}
+
+void	tearDown(void)
+{
+}
+
+void	_helper()
+{
+}
+
+void	test_(void){ }
+
+
+int	main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(test_);
+	return (UNITY_END());
+}
