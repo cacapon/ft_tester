3f6851a0a2f9e813e4b6049028b116cf5fab3fb6
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-10-31 13:43:19 +0900

    fix ft_isalnum(changed ChatGPT Created test)

--- test/libft/test_ft_isalnum.c
+++ test/libft/test_ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:49:10 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/31 12:39:54 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/10/31 13:42:40 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,35 +21,28 @@ void	tearDown(void)
 {
 }
 
-void	test_ft_isalnum_num(void)
+void test_ft_isalnum(void)
 {
-	TEST_ASSERT_EQUAL_INT(0, ft_isalnum('/'));
-	TEST_ASSERT_EQUAL_INT(1, ft_isalnum('0'));
-	TEST_ASSERT_EQUAL_INT(1, ft_isalnum('9'));
-	TEST_ASSERT_EQUAL_INT(0, ft_isalnum(':'));
-}
+    // 正しい入力のテスト
+    TEST_ASSERT_TRUE(ft_isalnum('A')); // 大文字
+    TEST_ASSERT_TRUE(ft_isalnum('z')); // 小文字
+    TEST_ASSERT_TRUE(ft_isalnum('0')); // 数字
+    TEST_ASSERT_TRUE(ft_isalnum('9')); // 数字
 
-void	test_ft_isalnum_upper(void)
-{
-	TEST_ASSERT_EQUAL_INT(0, ft_isalnum('@'));
-	TEST_ASSERT_EQUAL_INT(1, ft_isalnum('A'));
-	TEST_ASSERT_EQUAL_INT(1, ft_isalnum('Z'));
-	TEST_ASSERT_EQUAL_INT(0, ft_isalnum('['));
-}
+    // 不正な入力のテスト
+    TEST_ASSERT_FALSE(ft_isalnum(' ')); // スペース
+    TEST_ASSERT_FALSE(ft_isalnum('@')); // 記号
+    TEST_ASSERT_FALSE(ft_isalnum('\n')); // 改行
+    TEST_ASSERT_FALSE(ft_isalnum('!')); // 記号
 
-void	test_ft_isalnum_lower(void)
-{
-	TEST_ASSERT_EQUAL_INT(0, ft_isalnum('`'));
-	TEST_ASSERT_EQUAL_INT(1, ft_isalnum('a'));
-	TEST_ASSERT_EQUAL_INT(1, ft_isalnum('z'));
-	TEST_ASSERT_EQUAL_INT(0, ft_isalnum('{'));
+    // 境界条件のテスト
+    TEST_ASSERT_FALSE(ft_isalnum(256)); // ASCII範囲外の値
+    TEST_ASSERT_FALSE(ft_isalnum(-1)); // 負の値
 }
 
 int	main(void)
 {
 	UNITY_BEGIN();
-	RUN_TEST(test_ft_isalnum_num);
-	RUN_TEST(test_ft_isalnum_upper);
-	RUN_TEST(test_ft_isalnum_lower);
+	RUN_TEST(test_ft_isalnum);
 	return (UNITY_END());
 }
