c3f6f2bdb8b42e7703f3bc38fa2b0de1eaf02cf7
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-11-01 15:31:03 +0900

    fix ft_bzero test

--- test/libft/test_ft_bzero.c
+++ test/libft/test_ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:49:10 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/31 12:18:25 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/01 15:30:44 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,40 @@ void	tearDown(void)
 {
 }
 
-void	test_ft_bzero(void)
+void	_helper(size_t len, char expected_1st_c, char expected_end_c)
 {
-	char buf[10];
+	char	buf[10];
 
 	for (int i = 0; i < 10; i++)
-		buf[i] = 'A';
-	ft_bzero(buf, 10);
-	for (int i = 0; i < 10; i++)
-		TEST_ASSERT_EQUAL_CHAR(buf[i], 0);
+		buf[i] = 'x';
+	ft_bzero(buf, len);
+	TEST_ASSERT_EQUAL_CHAR(expected_1st_c, buf[0]);
+	TEST_ASSERT_EQUAL_CHAR(expected_end_c, buf[9]);
+}
+
+void	_helper_null(void)
+{
+}
+
+// buf[10] = "xxxxxxxxxx"をft_bzeroで変更して確認する。
+void	test_normal(void)
+{
+	_helper(10, '\0', '\0');
+}
+void	test_len_zero(void)
+{
+	_helper(0, 'x', 'x');
+}
+void	test_len_five(void)
+{
+	_helper(5, '\0', 'x');
 }
 
 int	main(void)
 {
 	UNITY_BEGIN();
-	RUN_TEST(test_ft_bzero);
+	RUN_TEST(test_normal);
+	RUN_TEST(test_len_zero);
+	RUN_TEST(test_len_five);
 	return (UNITY_END());
 }
