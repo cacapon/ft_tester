2ae1bdaea48aa43f662803b6f93e860775bf57dc
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-06 15:31:17 +0900

    test_ft_isascii

--- test/libft/test_ft_isascii.c
+++ test/libft/test_ft_isascii.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:49:10 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/31 13:57:52 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/06 15:30:57 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,31 @@ void	tearDown(void)
 {
 }
 
-void test_ft_isascii(void)
+void    test_normal()
 {
-    // 正しい入力のテスト
-    for (int i = 0; i < 128; i++) {
-        TEST_ASSERT_TRUE(ft_isascii(i)); // ASCII範囲内の値
-    }
+    TEST_ASSERT_TRUE(ft_isascii('A')); // 大文字
+    TEST_ASSERT_TRUE(ft_isascii('z')); // 小文字
+    TEST_ASSERT_TRUE(ft_isascii('0')); // 数字
+    TEST_ASSERT_TRUE(ft_isascii('9')); // 数字
+    TEST_ASSERT_TRUE(ft_isascii(' ')); // スペース
+    TEST_ASSERT_TRUE(ft_isascii('@')); // 記号
+    TEST_ASSERT_TRUE(ft_isascii('\n')); // 改行
+    TEST_ASSERT_TRUE(ft_isascii('!')); // 記号
+}
 
-    // 不正な入力のテスト
-    TEST_ASSERT_FALSE(ft_isascii(128)); // ASCII範囲外
-    TEST_ASSERT_FALSE(ft_isascii(255)); // ASCII範囲外
-    TEST_ASSERT_FALSE(ft_isascii(-1));  // 負の値
-
-    // 境界条件のテスト
-    TEST_ASSERT_TRUE(ft_isascii(0));    // ASCIIの最小値
-    TEST_ASSERT_TRUE(ft_isascii(127));  // ASCIIの最大値
+void    test_boundary()
+{
+    // ASCII
+    TEST_ASSERT_FALSE(ft_isascii(-1));
+    TEST_ASSERT_TRUE(ft_isascii(0));
+    TEST_ASSERT_TRUE(ft_isascii(127));
+    TEST_ASSERT_FALSE(ft_isascii(128));
 }
 
 int	main(void)
 {
 	UNITY_BEGIN();
-	RUN_TEST(test_ft_isascii);
+	RUN_TEST(test_normal);
+	RUN_TEST(test_boundary);
 	return (UNITY_END());
 }
