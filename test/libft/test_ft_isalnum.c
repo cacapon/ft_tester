0d1792dc2947f33a4cb5e7d3bfc497b5f812d6e9
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-06 15:20:48 +0900

    test_ft_isalnum

--- test/libft/test_ft_isalnum.c
+++ test/libft/test_ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:49:10 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/31 13:42:40 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/06 15:20:08 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,48 @@ void	tearDown(void)
 {
 }
 
-void test_ft_isalnum(void)
+void    test_normal_true()
 {
-    // 正しい入力のテスト
     TEST_ASSERT_TRUE(ft_isalnum('A')); // 大文字
     TEST_ASSERT_TRUE(ft_isalnum('z')); // 小文字
     TEST_ASSERT_TRUE(ft_isalnum('0')); // 数字
     TEST_ASSERT_TRUE(ft_isalnum('9')); // 数字
+}
 
-    // 不正な入力のテスト
+void    test_normal_false()
+{
     TEST_ASSERT_FALSE(ft_isalnum(' ')); // スペース
     TEST_ASSERT_FALSE(ft_isalnum('@')); // 記号
     TEST_ASSERT_FALSE(ft_isalnum('\n')); // 改行
     TEST_ASSERT_FALSE(ft_isalnum('!')); // 記号
+}
 
-    // 境界条件のテスト
-    TEST_ASSERT_FALSE(ft_isalnum(256)); // ASCII範囲外の値
-    TEST_ASSERT_FALSE(ft_isalnum(-1)); // 負の値
+void    test_boundary()
+{
+    // A-Z
+    TEST_ASSERT_FALSE(ft_isalnum(64));
+    TEST_ASSERT_TRUE(ft_isalnum(65));
+    TEST_ASSERT_TRUE(ft_isalnum(90));
+    TEST_ASSERT_FALSE(ft_isalnum(91));
+
+    // a-z
+    TEST_ASSERT_FALSE(ft_isalnum(64));
+    TEST_ASSERT_TRUE(ft_isalnum(65));
+    TEST_ASSERT_TRUE(ft_isalnum(90));
+    TEST_ASSERT_FALSE(ft_isalnum(91));
+    
+    // 0-9
+    TEST_ASSERT_FALSE(ft_isalnum(47));
+    TEST_ASSERT_TRUE(ft_isalnum(48));
+    TEST_ASSERT_TRUE(ft_isalnum(57));
+    TEST_ASSERT_FALSE(ft_isalnum(58));
 }
 
 int	main(void)
 {
 	UNITY_BEGIN();
-	RUN_TEST(test_ft_isalnum);
+	RUN_TEST(test_normal_true);
+	RUN_TEST(test_normal_false);
+	RUN_TEST(test_boundary);
 	return (UNITY_END());
 }
