216c7d50495cb1ddd2c9f3a1bdab5f031ab38b17
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-06 14:39:02 +0900

    test_atoi.c fix

--- test/libft/test_ft_atoi.c
+++ test/libft/test_ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:49:10 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/01 15:10:03 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/06 14:09:41 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,93 +26,47 @@ void	_helper(int expected, char *str)
 	TEST_ASSERT_EQUAL_INT(expected, ft_atoi(str));
 }
 
-void	_helper_null(void)
+void	test_normal(void)
 {
+	_helper(123, "123");
+	_helper(-456, "-456");
+	_helper(789, "   789");
+	_helper(-9876, "\t\t  \t-9876");
+	_helper(1, "1a2b3c");
 }
 
-void	test_1(void)
+void	test_ignore_str(void)
 {
-	_helper(1, "1");
+	_helper(0, "");
+	_helper(0, "ABC");
+	_helper(0, "+-123");
+	_helper(0, " ");
 }
-void	test_a1(void)
+
+void	test_overflow(void)
 {
-	_helper(0, "a1");
+	_helper(INT_MAX, "2147483648");
+	_helper(INT_MAX, "9223372036854775807");
 }
-void	test_2minus(void)
+
+void	test_underflow(void)
 {
-	_helper(0, "--1");
+	_helper(INT_MIN, "-2147483649");
+	_helper(INT_MIN, "-9223372036854775808");
 }
-void	test_2plus(void)
+
+void	test_str_is_null(void)
 {
-	_helper(0, "++1");
-}
-void	test_1plus(void)
-{
-	_helper(1, "+1");
-}
-void	test_1minus(void)
-{
-	_helper(-1, "-1");
-}
-void	test_zero(void)
-{
-	_helper(0, "0");
-}
-void	test_42tokyo(void)
-{
-	_helper(42, "+42Tokyo");
-}
-void	test_plus101(void)
-{
-	_helper(101, "+101");
-}
-void	test_intmax(void)
-{
-	_helper(INT_MAX, "2147483647");
-}
-void	test_intmin(void)
-{
-	_helper(INT_MIN, "-2147483648");
-}
-void	test_minus_plus(void)
-{
-	_helper(0, "-+42");
-}
-void	test_plus_minus(void)
-{
-	_helper(0, "+-42");
-}
-void	test_only_plus(void)
-{
-	_helper(0, "+");
-}
-void	test_only_minus(void)
-{
-	_helper(0, "-");
-}
-void	test_escape(void)
-{
-	_helper(42, "   \t\n42");
+	_helper(0, NULL);
 }
 
 int	main(void)
 {
 	UNITY_BEGIN();
-	RUN_TEST(test_1);
-	RUN_TEST(test_a1);
-	RUN_TEST(test_2minus);
-	RUN_TEST(test_2plus);
-	RUN_TEST(test_1plus);
-	RUN_TEST(test_1minus);
-	RUN_TEST(test_zero);
-	RUN_TEST(test_42tokyo);
-	RUN_TEST(test_plus101);
-	RUN_TEST(test_intmax);
-	RUN_TEST(test_intmin);
-	RUN_TEST(test_minus_plus);
-	RUN_TEST(test_plus_minus);
-	RUN_TEST(test_only_plus);
-	RUN_TEST(test_only_minus);
-	RUN_TEST(test_escape);
+	RUN_TEST(test_normal);
+	RUN_TEST(test_ignore_str);
+	RUN_TEST(test_overflow);
+	RUN_TEST(test_underflow);
+	RUN_TEST(test_str_is_null);
 	return (UNITY_END());
 }
