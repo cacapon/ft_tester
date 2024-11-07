ff16daa813bcc5ce14734fb0df196c3f7a6216c6
Cacapon Project <cacapon.project@gmail.com>
2024-11-07 10:33:39 +0900

    add test

--- test/libft/test_ft_strrchr.c
+++ test/libft/test_ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 14:53:31 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/10/31 19:03:28 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/07 10:30:46 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,41 +34,25 @@ void    _helper_null(char *s, char c)
     TEST_ASSERT_NULL(result);
 }
 
-void    test_normal(void)
-{
-    _helper_normal("Hello, World!", 'o');
-}
+void    test_found(void){_helper_normal("Hello, World",'o');}
+void    test_not_found_normal(void){_helper_null("hello",'x');}
+void    test_not_found_s_null(void){_helper_null(NULL,'o');}
+void    test_not_found_c_over(void){_helper_null("hello",128);}
+void    test_not_found_c_under(void){_helper_null("hello",-42);}
 
-void    test_invalid_char(void)
-{
-    _helper_null("Hello, World!", 'z');
-}
-
-void    test_1st_char(void)
-{
-    _helper_normal("Hello, World!", 'H');
-}
-
-void    test_last_char(void)
-{
-    _helper_normal("Hello, World!", '!');
-}
-
-void    test_empty_str(void)
-{
-    _helper_null("", 'a');
-}
-
-void    test_same_char(void)
-{
-    _helper_normal("aaaaa", 'a');
-}
+void    test_1st_char(void){_helper_normal("Hello, World!", 'H');}
+void    test_last_char(void){_helper_normal("Hello, World!", '!');}
+void    test_empty_str(void){_helper_null("", 'a');}
+void    test_same_char(void){_helper_normal("aaaaa", 'a');}
 
 int	main(void)
 {
 	UNITY_BEGIN();
-	RUN_TEST(test_normal);
-    RUN_TEST(test_invalid_char);
+	RUN_TEST(test_found);
+    RUN_TEST(test_not_found_normal);
+    RUN_TEST(test_not_found_s_null);
+    RUN_TEST(test_not_found_c_over);
+    RUN_TEST(test_not_found_c_under);
     RUN_TEST(test_1st_char);
     RUN_TEST(test_last_char);
     RUN_TEST(test_empty_str);
